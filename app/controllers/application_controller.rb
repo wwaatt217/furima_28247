608819b58fa15df6b46e612635b09247c6037933
wwaatt217 <68441754+wwaatt217@users.noreply.github.com>
2020-08-09 22:19:40 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == EVN["BASIC_AUTH_USER"] && password == EVN["BASIC_AUTH_PASSWORD"]
+    end
+   end
+
 end
