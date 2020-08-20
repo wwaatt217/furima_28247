55e1e5d5bb4d6d7239185ed933ae2522f9bb5241
wwaatt217 <68441754+wwaatt217@users.noreply.github.com>
2020-08-20 12:36:57 +0900

    単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "お名前は全角カナで入力してください。"}
   end
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   end
