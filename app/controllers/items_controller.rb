56c038a6f8d2713e8de9772125e0a91283e55aaf
wwaatt217 <68441754+wwaatt217@users.noreply.github.com>
2020-08-27 11:35:13 +0900

    編集・削除のリンク(出品者本人のみ使用可能)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,11 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order("created_at DESC")
   end
-
-  def edit
-  end
-
+  
   def new
     @item = Item.new
   end
-
+  
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -20,10 +17,16 @@ class ItemsController < ApplicationController
       render 'new'
     end
   end
-
+  
   def show
+    @item = Item.find(params[:id])
+  end
+  
+  def edit
   end
 
+  def destroy
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root  'items#index'
-  resources :items, only: [:show, :new, :edit, :create]
-  get 'items', to: 'items#seles'
+  resources :items, only: [:show, :new, :edit, :create, :destroy]
+  # get 'items', to: 'items#seles'
   
 end
