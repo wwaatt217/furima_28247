dc18bf6c99639f576126cfe7a1342806531dae76
wwaatt217 <68441754+wwaatt217@users.noreply.github.com>
2020-08-28 18:30:00 +0900

    編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root  'items#index'
-  resources :items, only: [:show, :new, :edit, :create, :destroy]
+  resources :items, only: [:show, :new, :edit, :create, :update, :destroy]
   # resources :buys, only: [:index]
   
 end
